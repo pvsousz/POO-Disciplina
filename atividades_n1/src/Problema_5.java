e0bb1bf3d0c201863bdedb4a43d1652be57e55ce
pvsousz <pauloviniciussous1@gmail.com>
2024-10-29 08:42:54 -0300

    Resolucao completa

--- atividades_n1/src/Problema_5.java
+++ atividades_n1/src/Problema_5.java
@@ -1,8 +1,116 @@
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 
 public class Problema_5 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
+        List<Integer> jogadores = new ArrayList<>();
+        Random random = new Random();
+        boolean contem = true;
+
+        for (int i = 1; i <= 16; i++) {
+            jogadores.add(i);            
+        }
+        Collections.shuffle(jogadores,random);
+        System.out.println("*********CAMPEONATO**********");
+        System.out.println("Lista de jogadores:"+jogadores);
+
+        while (contem) {
+            
+            if (jogadores.contains(7)&& jogadores.contains(1)) {
+
+
+                int index7 = jogadores.indexOf(7);
+                int index1 = jogadores.indexOf(1);
+                if (Math.abs(index7 - index1) == 1) {
+                    System.out.println("Próximos(ZHAO E Liu)");
+                }
+
+                
+                    // Oitavas de finais
+                jogadores.remove(random.nextInt(0,1)); 
+                jogadores.remove(random.nextInt(1,2));        
+                jogadores.remove(random.nextInt(2,3));
+                jogadores.remove(random.nextInt(3,4));
+                jogadores.remove(random.nextInt(4,5));
+                jogadores.remove(random.nextInt(5,6));
+                jogadores.remove(random.nextInt(6,7));
+                jogadores.remove(random.nextInt(7,8));
+                System.out.println("---------------Oitavas de Final----------------");
+                System.out.println("Lista de jogadores:"+jogadores);
+
+               
+               
+            }else{
+    
+                contem = false;
+                
+            }
+                //Quartas de finais
+            if (jogadores.contains(7)&& jogadores.contains(1))
+            
+            {
+                int index7 = jogadores.indexOf(7);
+                int index1 = jogadores.indexOf(1);
+                if (Math.abs(index7 - index1) == 1) {
+                    System.out.println("Próximos(ZHAO E Liu)");
+                }
+                jogadores.remove(random.nextInt(0,1));
+                jogadores.remove(random.nextInt(1,2));
+                jogadores.remove(random.nextInt(2,3));
+                jogadores.remove(random.nextInt(3,4));
+                System.out.println("---------------QUARTAS----------------");
+                System.out.println("Lista de jogadores:"+jogadores);
+
+               
+            }else{
+                
+                contem = false;
+            }
+                // SemiFinal
+            if (jogadores.contains(7)&& jogadores.contains(1))
+            {
+                int index7 = jogadores.indexOf(7);
+                int index1 = jogadores.indexOf(1);
+                if (Math.abs(index7 - index1) == 1) {
+                    System.out.println("Próximos(ZHAO E Liu)");
+                }
+                jogadores.remove(random.nextInt(0,1));
+                jogadores.remove(random.nextInt(1,2));
+                System.out.println("---------------SEMI----------------");
+                System.out.println("Lista de jogadores:"+jogadores);
+
+          
+            }else
+            {
+                
+                contem = false;
+            }    
+                // Final
+            if (jogadores.contains(7)&& jogadores.contains(1))
+            {   
+                int index7 = jogadores.indexOf(7);
+                int index1 = jogadores.indexOf(1);
+                if (Math.abs(index7 - index1) == 1) {
+                    System.out.println("Próximos (ZHAO E Liu)");
+                }
+                jogadores.remove(random.nextInt(0,1));
+                System.out.println("---------------GRAND FINALE----------------");
+                System.out.println("Lista de jogadores:"+jogadores);
+
+                
+            }else
+            {
+                
+                contem = false;
+            }   
+           
+        }
+         
+            
     }
+
 }
+
--- atividades_n1/src/Problema_6.java
+++ atividades_n1/src/Problema_6.java
@@ -1,9 +1,32 @@
 import java.util.Scanner;
 
 public class Problema_6 {
-    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         
+        System.out.println("Digite as coordenadas do primeiro retângulo (x0 y0 x1 y1): ");
+        double x0_1 = scanner.nextDouble();
+        double y0_1 = scanner.nextDouble();
+        double x1_1 = scanner.nextDouble();
+        double y1_1 = scanner.nextDouble();
+
+       
+        System.out.println("Digite as coordenadas do segundo retângulo (x0 y0 x1 y1): ");
+        double x0_2 = scanner.nextDouble();
+        double y0_2 = scanner.nextDouble();
+        double x1_2 = scanner.nextDouble();
+        double y1_2 = scanner.nextDouble();
+
+       
+        boolean naoInterseccionam = (x0_1 > x1_2) || (x1_1 < x0_2) || (y1_1 < y0_2) || (y0_1 > y1_2);
+
+        if (naoInterseccionam) {
+            System.out.println("Os retângulos não se interceptam.");
+        } else {
+            System.out.println("Os retângulos se interceptam.");
+        }
+
+        scanner.close();
     }
 }
